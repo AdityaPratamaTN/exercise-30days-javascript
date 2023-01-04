584056934545e7bf02547f137ba83b6d88b9e8dc
AdityaPratamaTN <adityapratama812@gmail.com>
2023-01-04 10:42:40 +0700

    first day

---
+++ Day 1 Introduction/main.js
@@ -0,0 +1,41 @@
+//comments can make code readable
+//Welcode to 30DaysOfJavaScript
+/*
+    comments can make code readable,
+    easy to reuse and informative
+*/
+
+let name = "Aditya Pratama";
+let learn = true;
+let udf;
+let girlfriend = null;
+
+console.log(typeof "Aditya Pratama");
+console.log(typeof 21);
+console.log(typeof true);
+console.log(typeof null);
+console.log(typeof undefined);
+
+let unnamed_var_1
+let unnamed_var_2
+let unnamed_var_3
+let unnamed_var_4
+
+let animal = "Cat";
+let life = 9;
+let meow = true;
+let fly = null;
+
+// let first_name = "Aditya";
+// let last_name = "Pratama";
+// let marital_status = "single";
+// let country = "Indonesia";
+// let age = 21;
+
+let first_name = "Aditya", last_name="Pratama", marital_status="single", country="Indonesia", age = 21;
+console.log(first_name, last_name, marital_status, country, age)
+
+let myAge = 21;
+let yourAge = 20;
+
+console.log("I am " + myAge + " years old.\nYou are " + yourAge + " years old.");
